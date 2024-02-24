b891e7ee32b57459d7d946b443171952893a0753
Aku <micedreams@micedreams-5.local>
2024-02-24 12:29:48 +0530

    Add details page.

--- lib/details.dart
+++ lib/details.dart
@@ -26,5 +26,45 @@ class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text(widget.config.city)),
-        body: Text('widget.config.city') );
+        body: FutureBuilder(
+            future: getWeather(widget.config.city),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              if ('SocketException' == snapshot.data) {
+                return const ListTile(
+                  title: Text('No internet'),
+                  subtitle: Text('Please try again later.'),
+                );
+              }
+
+              final city = snapshot.data;
+
+              return RefreshIndicator(
+                onRefresh: () async => await getWeather(widget.config.city),
+                child: ListView(
+                  children: <Widget>[
+                    ListTile(
+                      title: const Text('temperature'),
+                      subtitle: Text('${city.temperature} °C'),
+                    ),
+                    ListTile(
+                      title: const Text('weather'),
+                      subtitle: Text('${city.weather}'),
+                    ),
+                    ListTile(
+                      title: const Text('humidity'),
+                      subtitle: Text('${city.humidity} g.m-3'),
+                    ),
+                    ListTile(
+                      title: const Text('windSpeed'),
+                      subtitle: Text('${city.windSpeed} km/h'),
+                    ),
+                  ],
+                ),
+              );
+            }),
+      );
 }
